0cfc93f00dfb97d4be7a543d2f4ff6c919a1c665
Jakub Dziem <jakub.dziem9@gmail.com>
2024-11-04 18:11:47 +0100

    Lab5 zad1 i czesc zad 2

--- main.cpp
+++ main.cpp
@@ -1,51 +1,43 @@
 #include <iostream>
-#include "Array.h"
-template<auto divider, typename T>
-T showConvertingFromGrams(T elem) {
-    return elem/divider;
-}
-//template<typename T>
-//T minimum(T* tab, int n) {
-//    T min = tab[0];
-//    for(int i = 1; i < n; i++) {
-//        if(tab[i] > tab[i-1]) {
-//            min = tab[i];
-//        }
-//    }
-//    return min;
-//}
+#include <vector>
+#include <set>
+#include <algorithm>
 
+using namespace std;
+pair<int, int> func(vector<int> vector1) {
+    set<int> set1;
+    pair<int, int> pair1;
+    int sum = 0;
+    for(auto el : vector1) {
+        if(set1.contains(el)) {
+            pair1.first = el;
+        } else {
+            set1.insert(el);
+            sum += el;
+        }
+    }
+    pair1.second = sum;
+    return pair1;
+}
+template<typename T>
+set<T> checking(vector<T> vector1, vector<T> vector2) {
+    set<T> s1 (vector1.begin(), vector1.end());
+    set<T> s2 (vector2.begin(), vector2.end());
+    set<T> s1diff;
+    set<T> s2diff;
+    set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(), inserter(s1diff,s1diff.begin()));
+    set_difference(s2.begin(), s2.end(), s1.begin(), s1.end(), inserter(s2diff, s2diff.begin()));
+    //insert_iterator
+    //inserter
+    return s1diff.set_union(s2diff);
+}
 int main() {
-//    int tab[3] = {1,2,3};
-//    std::cout << minimum<int>(tab, 3) << std::endl;
-//    Array arr = Array<int>();
-//    arr.add(3);
-//    arr.add(4);
-//    arr.add(5);
-//    arr.add(8);
-//    arr.add(3);
-//    arr.add(3);
-//    arr.add(3);
-//    arr.add(3);
-//    arr.add(3);
-//    arr.add(10);
-//    arr.add(5);
-//    arr.show();
-//    arr.sort();
-//    std::cout << arr.atIndex(4) << std::endl;
-//    std::cout << arr.max() << std::endl;
-//    Array<std::string> arrS = Array<std::string>(4);
-//    arrS.add("abc");
-//    arrS.add("ab");
-//    arrS.add("12829");
-//    arrS.add("Jakub Dziem");
-//    arrS.sort();
-//    std::cout << arrS.max();
-    std::cout << showConvertingFromGrams<100,int>(1000) << std::endl;
-    std::cout << showConvertingFromGrams<100,float>(100.7) << std::endl;
-    std::cout << showConvertingFromGrams<100.1,float>(100.7) << std::endl;
+    vector<int> vector1 = {1, 2, 3, 4, 5, 5, 0, 11};
+    vector<int> vector2 = {1, 2, 3, 4, 5, 5, 9, 10};
+    checking(vector1, vector2);
+    pair<int, int> ab = func(vector1);
+    cout << ab.first << endl;
+    cout << ab.second << endl;
     return 0;
 }
 
-
-// zad3 auto divider, typename T
